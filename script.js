3f9e4515559926eec3ca51157b564a5b949e4775
SaiCharanU2005 <usaicharan24@gmail.com>
2025-05-15 21:02:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.querySelector("form").addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   alert("Thanks for contacting me!");
+// });
+// JavaScript functionality placeholder
+console.log("Portfolio site loaded");
+
